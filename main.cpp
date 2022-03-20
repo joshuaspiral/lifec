7a4a8ef8a1ed2a2c3892100cd0bea8479b5598d6
joshuaspiral <82312141+joshuaspiral@users.noreply.github.com>
2022-03-20 18:04:14 +0800

    Aesthetic changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Refactor code TODO 
 #include <iostream>
 #include <algorithm>
 #include <unistd.h>
@@ -6,19 +7,26 @@
 #define HEIGHT 50
 #define WIDTH 50
 #define SCREEN_HEIGHT 1000
-#define SCREEN_WIDTH 1000
 
 using namespace std;
 
-const int column_width = SCREEN_WIDTH / WIDTH;
-const int row_height = SCREEN_HEIGHT / HEIGHT;
+const int cellSize = SCREEN_HEIGHT / WIDTH;
+const int padding = cellSize * 0.10;
 
 int grid[HEIGHT][WIDTH];
 
-void resetGrid(int (*grid)[WIDTH]) {
+void clearGrid(int (*grid)[WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             grid[i][j] = 0;
+        }
+    }
+}
+
+void randomiseGrid(int (*grid)[WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            grid[i][j] = rand() % 2;
         }
     }
 }
@@ -49,22 +57,21 @@ void renderGrid(int (*grid)[WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             if (grid[i][j] == 1)
-                DrawRectangle(i * row_height, j * column_width, column_width, row_height, BLACK);
+                DrawRectangle(i * cellSize + padding, j * cellSize + padding, cellSize - padding, cellSize - padding, BLACK);
         }
     }
 }
 
 void nextGeneration(int (*grid)[WIDTH]) {
-    int oldgrid[HEIGHT][WIDTH], newGrid[HEIGHT][WIDTH];
+    int newGrid[HEIGHT][WIDTH];
 
-    std::copy(&grid[0][0], &grid[0][0] + HEIGHT * WIDTH, &oldgrid[0][0]);
     std::copy(&grid[0][0], &grid[0][0] + HEIGHT * WIDTH, &newGrid[0][0]);
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            int neighboursAlive = aliveNeighbours(oldgrid, i, j);
-            if ((neighboursAlive > 3 || (neighboursAlive < 2)) && oldgrid[i][j] == 1)
+            int neighboursAlive = aliveNeighbours(grid, i, j);
+            if ((neighboursAlive > 3 || (neighboursAlive < 2)) && grid[i][j] == 1)
                 newGrid[i][j] = 0;
-            if (neighboursAlive == 3 && oldgrid[i][j] == 0)
+            if (neighboursAlive == 3 && grid[i][j] == 0)
                 newGrid[i][j]= 1;
         }
     }
@@ -72,31 +79,26 @@ void nextGeneration(int (*grid)[WIDTH]) {
 }
 
 int main() {
-    cout << "Welcome to Conway's Game of Life\nTo reset the grid press R. To pause, press SPACE. To escape, press ESCAPE.\n";
-    bool pause = 0;
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Conway's Game of Life");
-    SetTargetFPS(60);
+    srand(time(0));
+    cout << "Welcome to Conway's Game of Life\nTo clear the grid press C. To randomise the grid, press R. To pause, press SPACE. To escape, press ESCAPE.\n";
+    bool pause = 1;
+    InitWindow(SCREEN_HEIGHT, SCREEN_HEIGHT, "Conway's Game of Life");
+    SetTargetFPS(30);
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_ESCAPE)) break;
         if (IsKeyPressed(KEY_SPACE)) pause = ((pause == 1) ? 0 : 1);
-        if (IsKeyPressed(KEY_R)) resetGrid(grid);
+        if (IsKeyPressed(KEY_C)) clearGrid(grid);
+        if (IsKeyPressed(KEY_R)) randomiseGrid(grid);
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
             int x, y;
             x = GetMouseX();
             y = GetMouseY();
-            if (x < SCREEN_WIDTH && x >= 0 && y < SCREEN_HEIGHT && y >= 0)
-                grid[x / row_height][y / column_width] = ((grid[y / row_height][x / column_width] == 1) ? 0 : 1);
+            if (x < SCREEN_HEIGHT && x >= 0 && y < SCREEN_HEIGHT && y >= 0)
+                grid[x / cellSize][y / cellSize] = ((grid[y / cellSize][x / cellSize] == 1) ? 0 : 1);
         }
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        for (int j = 1; j < WIDTH; j++) {
-            DrawLine(column_width * j, 0, column_width * j, SCREEN_HEIGHT, GRAY);
-        }
-        for (int i = 1; i < HEIGHT; i++) {
-            DrawLine(0, row_height * i, SCREEN_WIDTH, row_height * i, GRAY);
-        }
+        ClearBackground(GRAY);
 
         if (!pause) nextGeneration(grid);
         renderGrid(grid);
